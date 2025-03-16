2f7d27fbb4e1603231e946ae3ca3b29eb59604d7
sa-masum <sa.masum125@gmail.com>
2025-03-17 03:37:24 +0600

    MongoDB Connection

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 8000;
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://masum:semaphoreapp@cluster0.vozty.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB.")
+}).catch((error) => {
+    console.log("Error connecting to MongoDB-", error)
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port 8000...");
+})
